4e4298db0c028dd8a8001cb54c0fecd2376f1e24
Nicholas D. Socci <soccin@mskcc.org>
2022-07-16 16:33:16 -0400

    Added read_vcf func

---
+++ R/read_vcf.R
@@ -0,0 +1,62 @@
+require(readr)
+require(tidyr)
+require(purrr)
+
+parse_vcf_samples<-function(vi) {
+
+    ff=strsplit(vi$FORMAT[1],":")[[1]]
+    vi %>%
+        select(-FORMAT) %>%
+        gather(SAMPLE,GDATA,-VID) %>%
+        separate(GDATA,ff,sep=":") %>%
+        arrange(VID)
+
+}
+
+read_vcf<-function(vcfFile) {
+    vv=read_tsv(vcfFile,comment="##") %>%
+        rename(CHROM=`#CHROM`) %>%
+        mutate(VID=row_number()) %>%
+        select(VID,everything())
+
+    vs=select(vv,VID,10:ncol(vv)) %>%
+        group_split(FORMAT) %>%
+        map(parse_vcf_samples) %>%
+        bind_rows %>%
+        arrange(VID,SAMPLE)
+
+    vm=select(vv,1:9)
+
+    v0=vm %>%
+        select(VID,INFO) %>%
+        separate_rows(INFO,sep=";") %>%
+        filter(!grepl("=",INFO)) %>% mutate(VALUE="T")
+
+    v0flds=distinct(v0,INFO) %>% pull
+
+    v1=vm %>%
+        select(VID,INFO) %>%
+        separate_rows(INFO,sep=";") %>%
+        filter(grepl("=",INFO)) %>%
+        separate(INFO,c("INFO","VALUE"),sep="=")
+
+    vi=bind_rows(v0,v1) %>% spread(INFO,VALUE) %>% mutate_at(v0flds,~ifelse(is.na(.),FALSE,TRUE))
+    vm=vm %>% select(-INFO) %>% full_join(vi,by="VID")
+
+    ni=2
+    nj=3
+
+    while(!any(grepl("^#CHROM",readLines(vcfFile,n=nj)))) {
+        ii=nj
+        nj=ni+nj
+        ni=ii
+        print(nj)
+    }
+
+    header=readLines(vcfFile,n=nj)
+    hi=grep("^#CHROM",header)
+    header=header[1:(hi-1)]
+
+    list(header=header,vm=vm,vs=vs)
+
+}
